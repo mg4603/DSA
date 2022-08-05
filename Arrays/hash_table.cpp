c1d7aa8a828326a9e8b87ef76f132e1c62bb07a4
Michael George <michael.g.george@gmail.com>
2022-08-05 11:46:13 +0530

    Create a hash_table implementation

---
+++ Arrays/hash_table.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<functional>
+#include<string>
+using namespace std;
+
+template <typename T> 
+struct keyValue{
+    string key;
+    string value;
+};
+class {
+    
+    hash<string> hasher;
+    
+
+};
