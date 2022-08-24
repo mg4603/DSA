68eb23a193481bc5bd2c1651536068396d81ffff
Michael George <michael.g.george@gmail.com>
2022-08-24 15:45:43 +0530

    Create class to hold metadata in stack-min basic

---
+++ Stacks-queue/stack-min/basic.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+template <typename T, typename Q>
+class StackInfo{
+    T start, size, capacity;
+    public:
+        StackInfo(T start, T capacity){
+            this->start = start;
+            this->capacity = capacity;
+        }
+
+        
+
+        T getStart(){
+            return this->start;
+        }
+
+        T getSize(){
+            return this->size;
+        }
+
+        T getCapacity(){
+            return this->capacity;
+        }
+
+        void setStart(T start){
+            this->start = start
+        }
+
+        void incrementSize(){
+            this->size--;
+        }
+
+        void incrementCapacity(){
+            this->capacity++;
+        }
+
+        void decrementSize(){
+            this->size--;
+        }
+
+        void decrementCapacity(){
+            this->capacity--;
+        }
+
+};
