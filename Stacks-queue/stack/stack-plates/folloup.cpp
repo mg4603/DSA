ed4e39a2eef1e170260a1ec74de84e50f7268b62
Michael George <michael.g.george@gmail.com>
2022-08-26 18:31:14 +0530

    Add sol class in stack-plates follow

--- Stacks-queue/stack/stack-plates/folloup.cpp
+++ Stacks-queue/stack/stack-plates/folloup.cpp
@@ -3,7 +3,17 @@
 #include<stack>
 using namespace std;
 
-
+// T: size val type
+// Q: value type
+template<typename T, typename Q>
+class SetOfStack{
+    vector<Stack<Q>> stacks;
+    T capacity;
+    public:
+        SetOfStacks(T capacity){
+            this->capacity = capacity;
+        }    
+};
 
 int main(){
 
