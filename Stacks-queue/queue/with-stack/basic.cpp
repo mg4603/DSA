36ed6fc7e416374042fb976186f3a0e464df3fe0
Michael George <michael.g.george@gmail.com>
2022-08-29 17:16:50 +0530

    Implement queue with two stacks in with-stack

---
+++ Stacks-queue/queue/with-stack/basic.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+template <typename T>
+class Queue{
+    stack<T> oldest;
+    stack<T> newest;
+    
+    public:
+        Queue();
+
+       
+        
+};
+
+int main(){
+    return 0;
+}
