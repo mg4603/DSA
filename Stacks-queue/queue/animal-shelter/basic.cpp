3087dd73ec3743b65dd3ea4ada3bb82347cc6808
Michael George <michael.g.george@gmail.com>
2022-08-30 19:25:47 +0530

    Add abstract class Animal to animal-shelter

---
+++ Stacks-queue/queue/animal-shelter/basic.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+
+class Animal{
+    int order;
+    string name;
+    public:
+        Animal(string n){
+            name = n;    
+        }
+        
+};
+
+int main(){
+    return 0;
+}
