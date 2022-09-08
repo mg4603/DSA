1be93334218de7444c429bb230bb840e29ecc7b3
Michael George <michael.g.george@gmail.com>
2022-09-08 19:10:28 +0530

    Create fns to create minimal bst in minimal-tree

--- trees-graphs/trees/minimal-tree/basic.cpp
+++ trees-graphs/trees/minimal-tree/basic.cpp
@@ -25,7 +25,7 @@ struct Node{
         }
 };
 
-template<typename Q>
+template<typename T, typename Q>
 class Tree{
     Node<Q>* root;
     public:
@@ -33,9 +33,22 @@ class Tree{
         Node<Q>* getRoot(){
             return this->root;
         }
-        
+        void createMinimalBST(vector<Q> vec){
+            this->root = createMinimalBST(vec, 0, vec.size()-1);
+        }
+        Node<Q>* createMinimalBST(vector<Q> vec, T start, T end){
+            if(end < start){
+                return nullptr;
+            }
+            T mid((start+end)/2);
+            Node<Q>* n= = new Node(vec[mid]);
+            n->setLeft(createMinimalBST(vec, start, mid - 1));
+            n->setRight(createMinimalBST(vec, mid + 1, end));
+            return n;
+        }
 };
 
 int main(){
+    
     return 0;
 }
