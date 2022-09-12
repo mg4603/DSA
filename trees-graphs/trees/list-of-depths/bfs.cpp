88b3ab1fbacb40718f4f41059127055dd2c513ab
Michael George <michael.g.george@gmail.com>
2022-09-12 23:09:49 +0530

    Add test code to bfs list-of-depths

---
+++ trees-graphs/trees/list-of-depths/bfs.cpp
@@ -0,0 +1,21 @@
+#include<vector>
+#include<iostream>
+using namespace std;
+int main(){
+    Tree<int, int> *tree = new Tree<int, int>();
+    tree->insert(1);
+    tree->insert(2);
+    tree->insert(3);
+    tree->insert(4);
+    tree->insert(5);
+    tree->insert(6);
+    tree->insert(7);
+    tree->insert(8);
+    tree->insert(9);
+    tree->insert(10);
+    vector<LinkedList<int, int>*> lists;
+    tree->createLevelLinkedLists(lists);
+    lists[0]->print();
+    
+    return 0;
+}
