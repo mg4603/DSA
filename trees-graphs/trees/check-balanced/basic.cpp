67661901fb85c7185db8ec7c7868c75836953003
Michael George <michael.g.george@gmail.com>
2022-09-15 21:15:15 +0530

    Define Tree and TreeNode in check-balanced basic

---
+++ trees-graphs/trees/check-balanced/basic.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+template <typename T, typename Q>
+class Tree{
+    class TreeNode{
+        Q value;
+        TreeNode* left;
+        TreeNode* right;
+        public:
+            TreeNode(Q value){
+                this->value = value;
+            }
+    };
+    TreeNode* root;
+    public:
+        Tree():root(NULL){}
+
+};
+
+int main(){
+    return 0;
+}
