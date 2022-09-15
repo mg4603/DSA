d91fcb5f88d3826536ac7dcffd52a15ab9d7271d
Michael George <michael.g.george@gmail.com>
2022-09-16 02:15:39 +0530

    Test and debug check-balanced basic

--- trees-graphs/trees/check-balanced/basic.cpp
+++ trees-graphs/trees/check-balanced/basic.cpp
@@ -112,17 +112,9 @@ class Tree{
 
 int main(){
     Tree<int, int> *tree = new Tree<int, int>();
-    tree->insert(1);
     tree->insert(2);
     tree->insert(3);
-    tree->insert(4);
-    tree->insert(5);
-    tree->insert(6);
-    tree->insert(7);
-    tree->insert(8);
-    tree->insert(9);
-    tree->insert(10);
-    // cout<<tree->getRoot()->getValue();
+    tree->insert(1);
     // tree->inorder();
     cout<<tree->isBalanced();
     return 0;
