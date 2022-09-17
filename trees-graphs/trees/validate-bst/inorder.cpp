e58de5ba208b2a41bdbbe86f47f2cfa1904fe326
Michael George <michael.g.george@gmail.com>
2022-09-17 22:22:49 +0530

    Define Tree and Node Class in validate-bst inorder

---
+++ trees-graphs/trees/validate-bst/inorder.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+template <typename T, typename Q>
+class BinaryTree{
+    private:
+        class Node{
+            private:
+                Q value;
+                Node* left;
+                Node* right;
+            public:
+                Node(Q value){
+                    this->value = value;
+                }
+        };
+        Node* root;
+    public:
+        BinaryTree():root(nullptr){}
+};
+
+int main(){
+    return 0;
+}
