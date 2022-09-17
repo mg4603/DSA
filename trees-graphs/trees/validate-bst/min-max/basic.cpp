172567ac515dca85ba10bc5bcf107604a3b9a79a
Michael George <michael.g.george@gmail.com>
2022-09-18 03:44:37 +0530

    Define Tree and Node in validate-bst min-max

---
+++ trees-graphs/trees/validate-bst/min-max/basic.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+template <typename T, typename Q>
+class Tree{
+    private:
+        class Node{
+            private:
+                Q value;
+                Node *left;
+                Node *right;
+            public:
+                Node(Q value):left(nullptr), right(nullptr){
+                    this->value = value;
+                }
+        };
+    public:
+        Tree():root(nullptr){}
+};
+
+int main(){
+    return 0;
+}
