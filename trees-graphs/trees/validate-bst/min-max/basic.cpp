c6c52365cf21a981c622d3c844e96cb52775a9f7
Michael George <michael.g.george@gmail.com>
2022-09-18 04:15:58 +0530

    Test insert and inorderPrint Tree validate-bst min-max

--- trees-graphs/trees/validate-bst/min-max/basic.cpp
+++ trees-graphs/trees/validate-bst/min-max/basic.cpp
@@ -91,5 +91,10 @@ class Tree{
 };
 
 int main(){
+    Tree<int, int> *tree = new Tree<int, int>();
+    tree->insert(1);
+    tree->insert(2);
+    tree->insert(3);
+    tree->inorderPrint();
     return 0;
 }
