f4349e5806b174e30800345b252a7cc98fa9092d
Michael George <michael.g.george@gmail.com>
2022-09-18 03:52:50 +0530

    Add setter for left in validate-bst min-max

--- trees-graphs/trees/validate-bst/min-max/basic.cpp
+++ trees-graphs/trees/validate-bst/min-max/basic.cpp
@@ -25,6 +25,12 @@ class Tree{
                 void setValue(Q value){
                     this->value = value;
                 }
+                void setLeft(Node* left){
+                    if(this->getLeft() != nullptr){
+                        free(this->getLeft());
+                    }
+                    this->left = left;
+                }
         };
     public:
         Tree():root(nullptr){}
