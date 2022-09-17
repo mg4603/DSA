3b5fa6d888c65d6abc3cb658e69d2b4d5ce20c52
Michael George <michael.g.george@gmail.com>
2022-09-18 01:44:25 +0530

    Add setter for right in Node validate-bst 1_space

--- trees-graphs/trees/validate-bst/inorder/1_space.cpp
+++ trees-graphs/trees/validate-bst/inorder/1_space.cpp
@@ -22,6 +22,12 @@ class Tree{
                     }
                     this->left = left;
                 }
+                void setRight(Node* right){
+                    if(this.getRight() != nullptr){
+                        free(this.getRight());
+                    }
+                    this->right = right;
+                }
         };
 
         Node *root;
