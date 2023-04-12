e1b182b4e629abbec3aa4e6059fa87bf68f366ba
Michael George <michael.g.george@gmail.com>
2023-04-12 23:26:40 +0530

    Add parse_tests to tests_gen in codewars

--- codewars/tests_gen.py
+++ codewars/tests_gen.py
@@ -1,10 +1,19 @@
 from pathlib import Path
 from argparse import ArgumentParser
+from re import compile
 
 def get_tests(file_name):
     with Path(file_name).open('r') as file:
-        tests = file.readlines()
+        tests = file.read()
     return tests
+
+def parse_tests(tests):
+    regex = compile('Assert::That\((.+), Equals\((.+)\)\);')
+    tests = regex.findall(tests)
+    parsed_tests = ''
+    for test in tests:
+        parsed_tests += 'assert('+test[0]+' == '+test[1]+');\n'
+    return parsed_tests
 
 def parse_args():
     parser = ArgumentParser()
@@ -14,7 +23,7 @@ def parse_args():
 
 def main():
     args = parse_args()
-    file_name = args['file']
+    file_name = args.file
     tests = get_tests(file_name)
     parsed_tests = parse_tests(tests)
     print_tests(parsed_tests)
