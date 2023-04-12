84549f990b7bf191e3ea3558e7e68ee7d2250a87
Michael George <michael.g.george@gmail.com>
2023-04-12 23:11:50 +0530

    Add parse_args to tests_gen in codewars

--- codewars/tests_gen.py
+++ codewars/tests_gen.py
@@ -1,4 +1,11 @@
+from pathlib import Path
+from argparse import ArgumentParser
 
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument('file', 
+                        help='file containing sample tests from codewars.')
+    return parser.parse_args()
 
 def main():
     args = parse_args()
