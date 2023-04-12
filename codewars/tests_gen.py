a13d6729684cd452ca6ef454d3b610f33780c85f
Michael George <michael.g.george@gmail.com>
2023-04-12 23:10:15 +0530

    Add main to tests_gen in codewars

--- codewars/tests_gen.py
+++ codewars/tests_gen.py
@@ -1,4 +1,11 @@
 
 
+def main():
+    args = parse_args()
+    file_name = args['file']
+    tests = get_tests()
+    parsed_tests = parse_tests(tests)
+    print_tests(parsed_tests)
+
 if __name__ == '__main__':
     main()
