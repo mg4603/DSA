7a0fb3681980becaf826a80dbc7af12e210c7d55
Michael George <michael.g.george@gmail.com>
2023-04-12 23:13:51 +0530

    Add get_tests to tests_gen in codewars

--- codewars/tests_gen.py
+++ codewars/tests_gen.py
@@ -1,6 +1,11 @@
 from pathlib import Path
 from argparse import ArgumentParser
 
+def get_tests(file_name):
+    with Path(file_name).open('r') as file:
+        tests = file.readlines()
+    return tests
+
 def parse_args():
     parser = ArgumentParser()
     parser.add_argument('file', 
@@ -10,7 +15,7 @@ def parse_args():
 def main():
     args = parse_args()
     file_name = args['file']
-    tests = get_tests()
+    tests = get_tests(file_name)
     parsed_tests = parse_tests(tests)
     print_tests(parsed_tests)
 
