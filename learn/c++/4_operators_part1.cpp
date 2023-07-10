889228c684ae3f17a2b28b6d492a363819504cfe
Michael George <michael.g.george@gmail.com>
2023-07-10 17:39:19 +0530

    Add 4_operators_part1 to c++ in learn

---
+++ learn/c++/4_operators_part1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    // Operators
+    //      * works on one or more operands
+    //      * can be custom programmed to carry out desired operation in c++
+    //      eg:: "<<"
+    //          takes the string "Operators part1\n" and give it to the cout object
+    //          to be displayed to the console
+
+    std::cout << "Operators part 1\n";
+    return 0;
+}
