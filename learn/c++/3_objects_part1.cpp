2b8d2108430586c24b6618e96574ca21578eb4fb
Michael George <michael.g.george@gmail.com>
2023-07-10 17:34:17 +0530

    Add 3_objects_part1 to c++ in learn

---
+++ learn/c++/3_objects_part1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    // Object
+    //      objects describe things
+    //      instance of a class
+    //      eg: cout - describes the console output stream
+    //               - instance of ostream class
+    // Class
+    //      describes the structure of an object
+    //      similar to an cookie-cutter
+    std::cout << "Objects part 1\n";
+    return 0;
+}
