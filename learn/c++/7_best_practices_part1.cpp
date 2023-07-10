79fe4ecb2a156c63f629c7b6f0cac3cebcd3b178
Michael George <michael.g.george@gmail.com>
2023-07-10 23:09:04 +0530

    Add 7_best_practices_part1 to c++ in learn

---
+++ learn/c++/7_best_practices_part1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main()
+{
+    // indent by four spaces/ tab inside curly braces
+    // c++ is case sensitive - not good practices to use 
+    //                         same name with different case
+    // 
+    // 
+    // whitespaces are ignored by the cpp compiler
+    // except in key words - 
+    //      key words can't be separated by whitespaces (in  t => wrong)
+    // 
+    // 
+    // comments are ignored by the compiler
+    // code should be descriptive - 
+    //      use comments only when absolutely necessary for clarity
+    /*
+        multi-line
+        comment
+    */
+    return EXIT_SUCCESS;
+}
