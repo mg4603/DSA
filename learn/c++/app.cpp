505f13384973431b98f2947cc7e0a60f09d48235
Michael George <michael.g.george@gmail.com>
2023-07-13 20:03:28 +0530

    Create basic logic for game1 in app in c++ in learn

--- learn/c++/app.cpp
+++ learn/c++/app.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+void play_game1()
+{
+    int random(rand() % 251);
+    std::cout << "Guessing Game\n\n";
+    while(true)
+    {
+        int guess;
+        std::cout << "Guess a number between 0 and 250\n";
+        std::cin >> guess;
+        if(guess == random)
+        {
+            std::cout << "Congratulation! You win.\n\n";
+            break;
+        }
+        else if(guess > random)
+        {
+            std::cout << "Too high!\n";
+        }
+        else
+        {
+            std::cout << "Too low!\n";
+        }
+    }
+}
 
 int main()
 {
+    srand(time(NULL));
     int choice;
-
     do{
         std::cout << "0. Quit\n1. Play Game\n";
         std::cin >> choice;
@@ -11,10 +38,10 @@ int main()
         switch (choice)
         {
         case 0:
-            std::cout << "Thanks for nothing\ns";
+            std::cout << "\nThanks for nothing!\ns";
             break;
         case 1:
-            std::cout << "Game 1\n";
+            play_game1();
             break;
         default:
             break;
