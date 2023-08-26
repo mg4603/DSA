6ed04b626ba60ecde9a2370e30a745ef9982ade0
Michael George <michael.g.george@gmail.com>
2023-08-27 04:05:22 +0530

    Add convert_sorted_array_to_bst to leetcode

---
+++ leetcode/convert_sorted_array_to_bst/code.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <assert.h>
+
+#include <vector>
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+    TreeNode* sortedArrayToBST(std::vector<int> &nums, int left, int right)
+    {
+        if(left > right)
+        {
+            return nullptr;
+        }
+        int mid((left + right) / 2);
+        TreeNode *root = new TreeNode(nums[mid]);
+        root->left = sortedArrayToBST(nums, left, mid - 1);
+        root->right = sortedArrayToBST(nums, mid + 1, right);
+        return root;
+    }
+public:
+    TreeNode* sortedArrayToBST(std::vector<int>& nums) {
+        return sortedArrayToBST(nums, 0, nums.size() - 1);
+    }
+};
+
+
+int main()
+{
+    return 0;
+}
