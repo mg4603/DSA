8eda512bdcb5c6e62479ae4f9442d4316f660c05
Michael George <michael.g.george@gmail.com>
2024-03-21 01:26:20 +0530

    Add rm_nodes_linked_list to leetcode

---
+++ leetcode/rm_nodes_linked_list/code.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <assert.h>
+
+#include "../helper/list/list.cpp"
+
+class Solution {
+public:
+    ListNode<int>* removeNodes(ListNode<int>* head) {
+        if(!head)return nullptr;
+
+        head->next = removeNodes(head->next);
+        return head->next && head->val < head->next->val ? head->next : head;    }
+};
+
+int main()
+{
+    Solution sol;
+    {
+        ListNode<int> *head = to_list(std::vector<int>{5,2,13,3,8});
+        ListNode<int> *res = to_list(std::vector<int>{13, 8});
+        list_equal(sol.removeNodes(head), res);
+    }
+    return 0;
+}
