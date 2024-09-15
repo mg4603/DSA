c43fdf82d44a08e9c5610fe76e17b255d5a65656
michael <michael.g.george@gmail.com>
2024-09-15 08:04:04 +0530

    Add memoization solution to longest_repeated_subseq

--- learn/algorithms/dynamic_programming/problems/longest_repeated_subseq.cpp
+++ learn/algorithms/dynamic_programming/problems/longest_repeated_subseq.cpp
@@ -11,12 +11,31 @@ int longest_repeated_subseq(std::string s, int i, int j) {
                 longest_repeated_subseq(s, i , j + 1));
 }
 
+int memoLongestRepeatedSubseq(std::string s, int i, int j, 
+                    std::vector<std::vector<int>> &memo) {
+    
+    if(i == s.length() || j == s.length())return 0;
+
+    if(s[i] == s[j] && i != j) {
+        memo[i][j] = 1 + memoLongestRepeatedSubseq(s, i + 1, j + 1, memo);
+    }
+    else {
+        memo[i][j] = std::max(memoLongestRepeatedSubseq(s, i + 1, j, memo),
+                memoLongestRepeatedSubseq(s, i, j + 1, memo));
+
+    }
+    return memo[i][j];
+}
 
 int main() {
 
     std::string s = "ATACTCGGA";
-    std::cout << "The length of the longest repeated subsequnce is: " 
+    std::cout << "The length of the longest repeated subsequence is: " 
             << longest_repeated_subseq(s, 0, 0) << std::endl;
-                
+    
+    int n = s.length();
+    std::vector<std::vector<int>> memo(n, std::vector<int>(n, 0));
+    std::cout << "The length of the longest repeated subsequence(memo) is: " 
+            << memoLongestRepeatedSubseq(s, 0, 0, memo) << std::endl;
     return 0;
 }
