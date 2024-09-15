4820c3c2e65ba5f7a3a8d973171a01089db96484
michael <michael.g.george@gmail.com>
2024-09-15 10:48:38 +0530

    Add findLCS to diff_utility
    Added function to get table used to find longest common subsequence
    to diff_utility in dynamic_programming/problems.

--- learn/algorithms/dynamic_programming/problems/diff_utility.cpp
+++ learn/algorithms/dynamic_programming/problems/diff_utility.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 #include <vector>
 
+std::vector<std::vector<int>> findLCS(std::string s1, std::string s2) {
+    int n = s1.length();
+    int m = s2.length();
+    std::vector<std::vector<int>> table(n + 1, std::vector<int>(m + 1, 0));
 
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= m; j++) {
+            if(s1[i - 1] == s2[j - 1]) {
+                table[i][j] = table[i - 1][j - 1] + 1;
+            }else {
+                table[i][j] = std::max(table[i - 1][j], table[i][j - 1]);
+            }
+        }
+    }
+    return table;
+}
 
 int main() {
 
