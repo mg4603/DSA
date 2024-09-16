1a569d640a243a45d45b467a03b7cbcae09b8d45
michael <michael.g.george@gmail.com>
2024-09-16 10:27:54 +0530

    Add diff method to diff_util

--- learn/algorithms/dynamic_programming/problems/diff_utility.cpp
+++ learn/algorithms/dynamic_programming/problems/diff_utility.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 #include <vector>
 
+void diff(std::string s1, std::string s2, 
+        int n, int m, std::vector<std::vector<int>> &lookup) {
+    
+    if(m > 0 && n > 0 && s1[n - 1] == s2[m - 1]) {
+        diff(s1, s2, n - 1, m - 1, lookup);
+        std::cout << " " << s1[n - 1];
+    }else if(m > 0 && (n == 0 || lookup[n][m - 1] >= lookup[n - 1][m])) {
+        diff(s1, s2, n, m - 1, lookup);
+        std::cout << " +" << s2[m - 1];
+    }else if(n > 0 && (m == 0 || lookup[n - 1][m] > lookup[n][m - 1])) {
+        diff(s1, s2, n - 1, m, lookup);
+        std::cout << " -" << s1[n - 1];
+    }
+}
+
 std::vector<std::vector<int>> findLCS(std::string s1, std::string s2) {
     int n = s1.length();
     int m = s2.length();
@@ -19,6 +34,14 @@ std::vector<std::vector<int>> findLCS(std::string s1, std::string s2) {
 }
 
 int main() {
+    std::string s1 = "ABCDFGHJQZ";
+    std::string s2 = "ABCDEFGIJKRXYZ";
 
+    int n = s1.length();
+    int m = s2.length();
+
+    std::vector<std::vector<int>> lookup = findLCS(s1, s2);
+    diff(s1, s2, n, m, lookup);
+    std::cout << std::endl;
     return 0;
 }
