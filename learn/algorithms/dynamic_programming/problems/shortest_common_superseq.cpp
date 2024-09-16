7e6bd1cf597334f59dfb0f9ef66f4860a88e7441
michael <michael.g.george@gmail.com>
2024-09-17 01:15:59 +0530

    Memoize method to get shoretest common supersequence
    Added memoized method to get shortest common supersequence.

--- learn/algorithms/dynamic_programming/problems/shortest_common_superseq.cpp
+++ learn/algorithms/dynamic_programming/problems/shortest_common_superseq.cpp
@@ -18,12 +18,42 @@ int shortestCommonSupersequenceLength(
         shortestCommonSupersequenceLength(s1, s2, m, n - 1));
 }
 
+int memoShortestCommonSupersequenceLength(
+        std::string s1, std::string s2,
+        int m, int n, 
+        std::vector<std::vector<int>> &memo) {
+
+        if(m == 0 || n == 0) {
+            return m + n;
+        }
+
+        if(memo[m][n] != 0)return memo[m][n];
+
+        if(s1[m - 1] == s2[n - 1]) {
+            memo[m][n] = 1 + memoShortestCommonSupersequenceLength(s1, s2,
+                            m - 1, n - 1, memo);
+        }else {
+            memo[m][n] = 
+                std::min(
+                  memoShortestCommonSupersequenceLength(
+                    s1, s2, m - 1, n, memo),
+                  memoShortestCommonSupersequenceLength(
+                    s1, s2, m, n - 1, memo));
+        }
+        return memo[m][n];
+}
+
 int main() {
     std::string s1 = "ABCBDAB";
     std::string s2 = "BDCABA";
     int m = s1.length(), n = s2.length();
     std::cout << "Len of shortest common supersequence: " 
             << shortestCommonSupersequenceLength(s1, s2, m, n) 
+            << std::endl;
+    
+    std::vector<std::vector<int>> memo(m + 1, std::vector<int>(n + 1, 0));
+    std::cout << "Len of shortest common supersequence: " 
+            << memoShortestCommonSupersequenceLength(s1, s2, m, n, memo)
             << std::endl;
     return 0;
 }
