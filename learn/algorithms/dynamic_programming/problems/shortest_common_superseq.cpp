43db97a2cb786ca96e6b9391c75f83872ab81a9f
michael <michael.g.george@gmail.com>
2024-09-17 11:34:22 +0530

    Create tabulated solution to SCS

--- learn/algorithms/dynamic_programming/problems/shortest_common_superseq.cpp
+++ learn/algorithms/dynamic_programming/problems/shortest_common_superseq.cpp
@@ -43,6 +43,34 @@ int memoShortestCommonSupersequenceLength(
         return memo[m][n];
 }
 
+int tableShortestCommonSupersequenceLength(
+    std::string s1, std::string s2,
+    int m, int n, std::vector<std::vector<int>> &table) {
+    
+    for(int i = 0; i <= m; i++) {
+        table[i][0] = 0;
+    }
+
+    for(int j = 0; j <= n; j++) {
+        table[0][j] = 0;
+    }
+
+    for(int i = 1; i <= m; i++) {
+        for(int j = 1; j <= n; j++) {
+            if(s1[i - 1] == s2[j - 1]) {
+                table[i][j] = 1 + table[i - 1][j - 1];
+            }else {
+                if(table[i - 1][j] < table[i][j - 1]) {
+                    table[i][j] = 1 + table[i - 1][j];
+                }else {
+                    table[i][j] = 1 + table[i][j - 1];
+                }
+            }
+        }
+    }
+    return table[m][n];
+}
+
 int main() {
     std::string s1 = "ABCBDAB";
     std::string s2 = "BDCABA";
@@ -52,7 +80,12 @@ int main() {
             << std::endl;
     
     std::vector<std::vector<int>> memo(m + 1, std::vector<int>(n + 1, 0));
-    std::cout << "Len of shortest common supersequence: " 
+    std::cout << "Len of shortest common supersequence(memo): " 
+            << memoShortestCommonSupersequenceLength(s1, s2, m, n, memo)
+            << std::endl;
+
+    std::vector<std::vector<int>> table(m + 1, std::vector<int>(n + 1, 0));
+    std::cout << "Len of shortest common supersequence(table): " 
             << memoShortestCommonSupersequenceLength(s1, s2, m, n, memo)
             << std::endl;
     return 0;
