eb02749b088abcf1fb583181a988decb8297df2e
michael <michael.g.george@gmail.com>
2024-09-19 16:33:58 +0530

    Add dynamic solution to longest increasing subsequence

--- learn/algorithms/dynamic_programming/problems/longest_increasing_subseq.cpp
+++ learn/algorithms/dynamic_programming/problems/longest_increasing_subseq.cpp
@@ -13,6 +13,25 @@ int lis(std::vector<int> nums, int i, int n, int prev) {
     }
     return std::max(inc, exc);
 }
+
+int dynamic_lis(std::vector<int> &nums) {
+    
+    int n = nums.size();
+    std::vector<int> table(n, 0);
+    table[0] = 1;
+    int res = 0;
+    for(int i = 1; i < n; i++) {
+        for(int j = 0; j < i; j++) {
+            if(nums[j] < nums[i] && table[j] > table[i]) {
+                table[i] = table[j];
+            }
+        }
+        table[i]++;
+        res = std::max(res, table[i]);
+    }
+    return res;
+}
+
 int main() {
 
     std::vector<int> nums = {0, 8, 4, 12, 2, 10, 6, 14, 
@@ -20,6 +39,10 @@ int main() {
     int n = nums.size();
 
     std::cout << "Length of longest increasing subsequence: " 
-            << lis(nums, 0, n, INT_MIN) << std::endl;;
+            << lis(nums, 0, n, INT_MIN) << std::endl;
+
+    std::vector<int> memo(n, 0);
+    std::cout << "Length of longest increasing subsequence(dynamic): " 
+            << dynamic_lis(nums) << std::endl;
     return 0;
 }
