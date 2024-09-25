3afd7de9ce602389a850096c620d1e073e874dd4
michael <michael.g.george@gmail.com>
2024-09-25 15:37:54 +0530

    Add method to find maximum increasing subsequence

--- learn/algorithms/dynamic_programming/problems/max_sum_inc_subseq.cpp
+++ learn/algorithms/dynamic_programming/problems/max_sum_inc_subseq.cpp
@@ -44,6 +44,42 @@ int dynamicMaxSumIncreasingSubseq(std::vector<int> &nums) {
     return res;
 }
 
+void findMaxSumIncreasingSubseq(std::vector<int> &nums) {
+    int n = nums.size();
+    if(n == 0)return;
+
+    std::vector<std::vector<int>> table(n);
+    std::vector<int> sums(n, 0);
+
+    sums[0] = nums[0];
+    table[0].push_back(nums[0]);
+
+    for(int i = 1; i < n; i++) {
+        for(int j = 0; j < i; j++) {
+            if(nums[i] > nums[j] && sums[i] < sums[j]) {
+                sums[i] = sums[j];
+                table[i] = table[j];
+            }
+        }
+        sums[i] += nums[i];
+        table[i].push_back(nums[i]);
+    }
+
+    int maxSum = 0;
+    std::vector<int> res;
+    for(int i = 0; i < n; i++) {
+        if(sums[i] > maxSum) {
+            maxSum = sums[i];
+            res = table[i];
+        }
+    }
+
+    for(int &num: res) {
+        std::cout << num << ' ' ;
+    }
+    std::cout << std::endl;
+}
+
 int main() {
     std::vector<int> nums = {0, 8, 4, 12, 2, 10, 6, 14, 
                              1, 9, 5, 13, 3, 11};
@@ -57,5 +93,7 @@ int main() {
                 << dynamicMaxSumIncreasingSubseq(nums)
                 << std::endl;;
     
+    std::cout << "Max sum increasing subsequence: " << std::endl;
+    findMaxSumIncreasingSubseq(nums);
     return 0;
 }
