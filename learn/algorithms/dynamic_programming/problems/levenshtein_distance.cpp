7faab8632871640ef4bdcbbe3e3505329a003d1b
michael <michael.g.george@gmail.com>
2024-09-27 02:26:22 +0530

    Add method to find levenshtein distance

--- learn/algorithms/dynamic_programming/problems/levenshtein_distance.cpp
+++ learn/algorithms/dynamic_programming/problems/levenshtein_distance.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+
+int levenshteinDistance(std::string s1, std::string s2, 
+        int n, int m) {
+
+    if(n == 0 || m == 0){
+        return std::max(n, m);
+    }
+
+    if(s1[n - 1] == s2[m - 1]) {
+        return levenshteinDistance(s1, s2, n - 1, m - 1);
+    }else {
+        return 1 + std::min({levenshteinDistance(s1, s2, n - 1, m),
+                        levenshteinDistance(s1, s2, n, m - 1),
+                        levenshteinDistance(s1, s2, n - 1, m - 1)});
+    }
+}
 
 int main() {
     std::string s1 = "kitten";
