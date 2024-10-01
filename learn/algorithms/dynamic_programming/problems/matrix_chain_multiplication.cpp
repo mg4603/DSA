a6d7f0a204333571c2f47b3255eaa4e8e525dc57
michael <michael.g.george@gmail.com>
2024-10-01 16:43:55 +0530

    Add dynamic solution to matrix chain multiplication

--- learn/algorithms/dynamic_programming/problems/matrix_chain_multiplication.cpp
+++ learn/algorithms/dynamic_programming/problems/matrix_chain_multiplication.cpp
@@ -15,12 +15,37 @@ int matrixChainMultiplication(std::vector<int> &dims, int i, int j) {
     return min_cost;
 }
 
+int dynamicMatrixChainMultiplication(std::vector<int> &dims, 
+                int i, int j, std::vector<std::vector<int>> &table) {
+
+    if(j <= i + 1)return 0;
+    if(table[i][j] != 0)return table[i][j];
+
+    int min_cost = INT_MAX;
+
+    for(int k = i + 1; k < j; k++) {
+        int cost = dynamicMatrixChainMultiplication(dims, i, k, table);
+        cost += dynamicMatrixChainMultiplication(dims, k, j, table);
+        cost += (dims[i] * dims[k] * dims[j]);
+        min_cost = std::min(cost, min_cost);
+    }
+    
+    table[i][j] = min_cost;
+    return table[i][j];
+}
+
 int main() {
     std::vector<int> dims = {10, 30, 5, 60};
     int n = dims.size();
 
     std::cout << "The minimum cost is: " 
             << matrixChainMultiplication(dims, 0, n - 1)
+            << std::endl;;
+    
+    std::vector<std::vector<int>> table(n + 1, 
+                                std::vector<int>(n + 1, 0));
+    std::cout << "The minimum cost(dynamic) is: " 
+            << dynamicMatrixChainMultiplication(dims, 0, n - 1, table)
             << std::endl;;
     return 0;
 }
