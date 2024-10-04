b2dd8be8fd75b41ef325360c8e32bdb189197694
michael <michael.g.george@gmail.com>
2024-10-04 21:15:20 +0530

    Add fn to find min cost to reach last cell from first

--- learn/algorithms/dynamic_programming/problems/min_cost_reach_last_cell_mat_first_cell.cpp
+++ learn/algorithms/dynamic_programming/problems/min_cost_reach_last_cell_mat_first_cell.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 #include <vector>
+#include <climits>
+
+int findMinCost(std::vector<std::vector<int>> &grid, int n, int m) {
+    if(n == 0 || m == 0)return INT_MAX;
+
+    if(n == 1 && m == 1)return grid[0][0];
+    return std::min(findMinCost(grid, n - 1, m), 
+            findMinCost(grid, n, m - 1)) + grid[n - 1][m - 1];
+}
 
 int main() {
+
+    std::vector<std::vector<int>> grid = {
+        { 4, 7, 8, 6, 4 },
+        { 6, 7, 3, 9, 2 },
+        { 3, 8, 1, 2, 4 },
+        { 7, 1, 7, 3, 7 },
+        { 2, 9, 8, 9, 3 }
+    };
+    int n = grid.size();
+    int m = grid[0].size();
+    std::cout << "The minimum cost is: " 
+        << findMinCost(grid, n, m) 
+        << std::endl;;
     return 0;
 }
