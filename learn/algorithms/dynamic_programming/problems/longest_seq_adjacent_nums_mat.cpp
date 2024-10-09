be0a6f498d4aa94e21a526fc09099c94437c486b
michael <michael.g.george@gmail.com>
2024-10-09 07:36:33 +0530

    Add fn to find longest seq formed by adjacent nums in mat

--- learn/algorithms/dynamic_programming/problems/longest_seq_adjacent_nums_mat.cpp
+++ learn/algorithms/dynamic_programming/problems/longest_seq_adjacent_nums_mat.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 #include <vector>
 
+std::vector<int> findLongestSeq(
+                std::vector<std::vector<int>> &mat) {
+    int n = mat.size();
+    std::vector<int> longestSeq;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            std::vector<int> seq = findLongestSeq(mat, i, j);
+            if(longestSeq.size() > seq.size()) {
+                longestSeq = seq;
+            }
+        }
+    }
+    return longestSeq;
+}
+
 template <typename T>
 void printVector(std::vector<T> &vec) {
     for(T it: vec) {
@@ -10,6 +25,15 @@ void printVector(std::vector<T> &vec) {
 }
 
 int main() {
-
+    std::vector<std::vector<int>> mat =
+    {
+        { 10, 13, 14, 21, 23 },
+        { 11, 9, 22, 2, 3 },
+        { 12, 8, 1, 5, 4 },
+        { 15, 24, 7, 6, 20 },
+        { 16, 17, 18, 19, 25 }
+    };
+    std::vector<int> res = findLongestPath(mat);
+    printVector(res);
     return 0;
 }
