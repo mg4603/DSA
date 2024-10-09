00ceb1b54d651ccdd9e29f076afff6999154fae0
michael <michael.g.george@gmail.com>
2024-10-09 07:23:40 +0530

    Add fn to print longest seq formed by adj nums in mat

--- learn/algorithms/dynamic_programming/problems/longest_seq_adjacent_nums_mat.cpp
+++ learn/algorithms/dynamic_programming/problems/longest_seq_adjacent_nums_mat.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 #include <vector>
 
+template <typename T>
+void printVector(std::vector<T> &vec) {
+    for(T it: vec) {
+        std::cout << it << ' ';
+    }
+    std::cout << std::endl;
+}
+
 int main() {
+
     return 0;
 }
