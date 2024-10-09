efede2b590fb75277855acca7be041d38a5fc09f
michael <michael.g.george@gmail.com>
2024-10-10 05:06:19 +0530

    Add dynamic fn to find longest seq from each cell of mat

--- learn/algorithms/dynamic_programming/problems/longest_seq_adjacent_nums_mat.cpp
+++ learn/algorithms/dynamic_programming/problems/longest_seq_adjacent_nums_mat.cpp
@@ -50,12 +50,58 @@ std::vector<int> findLongestSeq(
 }
 
 std::vector<int> dynamicFindLongestSeq(
+    std::vector<std::vector<int>> &mat, int i, int j,
+    std::vector<std::vector<std::vector<int>>> &memo){
+    
+    int n = mat.size();
+    std::vector<int> longestSeq;
+    if(i < 0 || j < 0 || i >= n || j >= n)return longestSeq;
+    if(memo[i][j].size() > 0)return memo[i][j];
+    
+    if(i > 0 && mat[i - 1][j] == mat[i][j] + 1) {
+        std::vector<int> seq = dynamicFindLongestSeq(
+                                    mat, i - 1, j, memo);
+        if(longestSeq.size() < seq.size()) {
+            longestSeq = seq;
+        }
+    }
+
+    if(i + 1 < n && mat[i + 1][j] == mat[i][j] + 1) {
+        std::vector<int> seq = dynamicFindLongestSeq(
+                                    mat, i + 1, j, memo);
+        if(longestSeq.size() < seq.size()) {
+            longestSeq = seq;
+        }
+    }
+
+    if(j > 0 && mat[i][j - 1] == mat[i][j] + 1) {
+        std::vector<int> seq = dynamicFindLongestSeq(
+                                    mat, i, j - 1, memo);
+        if(longestSeq.size() < seq.size()) {
+            longestSeq = seq;
+        }
+    }
+
+    if(j + 1 < n && mat[i][j + 1] == mat[i][j] + 1) {
+        std::vector<int> seq = dynamicFindLongestSeq(
+                                    mat, i, j + 1, memo);
+        if(longestSeq.size() < seq.size()) {
+            longestSeq = seq;
+        }
+    }
+    
+    longestSeq.push_back(mat[i][j]);
+    memo[i][j] = longestSeq;
+    return memo[i][j];
+}
+
+
+std::vector<int> dynamicFindLongestSeq(
         std::vector<std::vector<int>> &mat) {
     
     int n = mat.size();
     std::vector<std::vector<std::vector<int>>> memo(n, std::vector<std::vector<int>>(n));
-
-    std::vector<int> longestSeq = {};
+    std::vector<int> longestSeq;
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
             std::vector<int> seq = dynamicFindLongestSeq(mat, i, j, memo);
@@ -64,6 +110,7 @@ std::vector<int> dynamicFindLongestSeq(
             }
         }
     }
+    std::reverse(longestSeq.begin(), longestSeq.end());
     return longestSeq;
 }
 template <typename T>
@@ -83,7 +130,12 @@ int main() {
         { 15, 24, 7, 6, 20 },
         { 16, 17, 18, 19, 25 }
     };
+    std::cout << "LongestSeq: " << std::endl;;
     std::vector<int> res = findLongestSeq(mat);
+    printVector(res);
+
+    std::cout << "Longest Seq(dynamic): " << std::endl;;
+    res = dynamicFindLongestSeq(mat);
     printVector(res);
     return 0;
 }
