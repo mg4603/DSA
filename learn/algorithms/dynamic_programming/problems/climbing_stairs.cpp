bb47142ab1637f8fe17d8352b3fd84add9c227f0
michael <michael.g.george@gmail.com>
2024-10-13 18:44:57 +0530

    Solve climbing stairs problem

--- learn/algorithms/dynamic_programming/problems/climbing_stairs.cpp
+++ learn/algorithms/dynamic_programming/problems/climbing_stairs.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 #include <vector>
 
+/*
+    Problem:
+    Find the distinct number of ways to reach a certain 
+    stair, if on any stair, we can climb take either
+    one or two steps.
+
+    Objective fn:
+    f(i) is the distinct number of ways to get to the ith stair
+
+    Recurrence Relation:
+    f(i) = f(i - 1) + f(i - 2)
+
+    Base case:
+        f(0) = 1 => do nothing
+        f(1) = 1
+*/
+
+
+
 int main() {
-    
+
     return 0;
 }
