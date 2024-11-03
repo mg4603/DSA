728744447d1469d90c48c0ae7f455d38550895b1
michael <michael.g.george@gmail.com>
2024-11-03 09:36:45 +0530

    Add function to get input in learn go

--- learn_go/main.go
+++ learn_go/main.go
@@ -4,7 +4,24 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
+
+func getInput(reader *bufio.Reader, prompt string) (string, error) {
+	fmt.Print(prompt)
+
+	// returns input and error on reading from specified source
+	//  stdin in this case
+
+	// reads everything the user has typed in up until they
+	// 	entered a specific character
+
+	// reader reads after user has entered a new line (hit enter)
+
+	input, err := reader.ReadString('\n')
+	input = strings.TrimSpace(input)
+	return input, err
+}
 
 func createBill() bill {
 	reader := bufio.NewReader(os.Stdin)
