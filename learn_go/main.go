148ac104cf3f9c3ade6c373639b35ff1aa3601db
michael <michael.g.george@gmail.com>
2024-11-03 07:09:53 +0530

    Add function to create bill in learn go

--- learn_go/main.go
+++ learn_go/main.go
@@ -1,45 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
-// formating string, printing
-// messages to console
+func createBill() bill {
+	reader := bufio.NewReader(os.Stdin)
+
+	name, _ := getInput(reader, "Create a new bill name: ")
+
+	bill := newBill(name)
+	fmt.Println("Created the bill - ", bill.name)
+	return bill
+}
 
 func main() {
-	// numbersStringVariables()
-	// printingFormattingString()
-	// arraySlices()
-	// stringPackage()
-	// sortPackage()
-	// loops()
-	// booleansConditionals()
-	// sayGreeting("World")
-
-	// > pass reference to function sayGreeting
-	// 		to function cycleNames
-	// > don't invoke function
-	// names := []string{"name1", "name2", "name3"}
-	// cycleNames(names, sayGreeting)
-	// cycleNames(names, sayBye)
-
-	// a1 := circleArea(10.5)
-	// a2 := circleArea(15)
-	// fmt.Printf("Circle1 is %0.3f\n", a1)
-	// fmt.Printf("Circle2 is %0.3f\n", a2)
-
-	// fn, ln := getInitials("name surname")
-	// fmt.Println(fn, ln)
-	//
-	// fn2, ln2 := getInitials("name2 middlename2 surname2")
-	// fmt.Println(fn2, ln2)
-	//
-	// fn3, ln3 := getInitials("name3")
-	// fmt.Println(fn3, ln3)
-	// mapsTutorial()
-
-	str := "String"
-	fmt.Println(str)
-	pointerTutorial(&str)
-	fmt.Println(str)
+	myBill := createBill()
+	// a copy of mybill is passed into
+	// 	receiver function format
+	fmt.Println(myBill.format())
 
 }
