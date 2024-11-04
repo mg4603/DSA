debda634b08e3a08fe86de276ae29ae065e33d18
michael <michael.g.george@gmail.com>
2024-11-04 12:53:44 +0530

    Add function to prompt for options in learn go

--- learn_go/main.go
+++ learn_go/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -33,10 +34,56 @@ func createBill() bill {
 	return bill
 }
 
+func promptOptions(b *bill) {
+	reader := bufio.NewReader(os.Stdin)
+
+	option, _ := getInput(
+		reader,
+		"Choose option (a - add item, s - save bill, t - add tip): ")
+
+	switch option {
+	case "a":
+		item_name, _ := getInput(reader, "Item Name: ")
+		price_string, _ := getInput(reader, "Item price: ")
+
+		price, err := strconv.ParseFloat(price_string, 64)
+
+		if err != nil {
+			fmt.Println("The price must be a number")
+			promptOptions(b)
+			return
+		}
+
+		b.addItem(item_name, price)
+
+		fmt.Println("item added - ", item_name, price)
+		promptOptions(b)
+	case "t":
+		tip_string, _ := getInput(reader, "Enter tip amount ($): ")
+
+		tip, err := strconv.ParseFloat(tip_string, 64)
+
+		if err != nil {
+			fmt.Println("The tip must be a number")
+			promptOptions(b)
+			return
+		}
+
+		b.updateTip(tip)
+		fmt.Println("tip added - ", tip)
+		promptOptions(b)
+	case "s":
+		b.save()
+		fmt.Println("You saved the bill - ", b.name)
+	default:
+		fmt.Println("that was not a valid option...")
+		promptOptions(b)
+	}
+}
+
 func main() {
 	myBill := createBill()
-	// a copy of mybill is passed into
-	// 	receiver function format
-	fmt.Println(myBill.format())
+
+	promptOptions(&myBill)
 
 }
