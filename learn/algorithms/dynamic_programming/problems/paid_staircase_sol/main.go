4ce9112a1a9949750b53b018358185bc13eece17
michael <michael.g.george@gmail.com>
2024-11-19 13:44:30 +0530

    Add reverse slice to paid_staircase_sol

---
+++ learn/algorithms/dynamic_programming/problems/paid_staircase_sol/main.go
@@ -0,0 +1,19 @@
+package paidstaircasesol
+
+/*
+Problem:
+
+	Find cheapest path to the top of staircase if
+	each step has a cost and you can climb 1...k
+	stairs at any step
+*/
+
+func reverseSlice(s []int) []int {
+	if len(s) == 0 {
+		return s
+	}
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+	return s
+}
