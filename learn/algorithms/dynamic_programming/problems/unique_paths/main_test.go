c97f184c9c41cbd501eba1a5543b70657d099b3b
michael <michael.g.george@gmail.com>
2024-11-20 19:48:14 +0530

    Test add to hashset in unique_paths

--- learn/algorithms/dynamic_programming/problems/unique_paths/main_test.go
+++ learn/algorithms/dynamic_programming/problems/unique_paths/main_test.go
@@ -1,6 +1,8 @@
 package uniquepaths
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_uniquePaths(t *testing.T) {
 	type args struct {
@@ -60,4 +62,55 @@ func Test_uniquePaths(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestHashset_Add(t *testing.T) {
+	tests := []struct {
+		name        string
+		intputPairs []Pair
+		checkPairs  []Pair
+		expected    []bool
+	}{
+		{
+			name: "Add Unique Pairs",
+			intputPairs: []Pair{
+				{i: 1, j: 2},
+				{i: 3, j: 4},
+			},
+			checkPairs: []Pair{
+				{i: 1, j: 2},
+				{i: 3, j: 4},
+				{i: 2, j: 3},
+			},
+			expected: []bool{true, true, false},
+		},
+		{
+			name: "Add Duplicate Pairs",
+			intputPairs: []Pair{
+				{i: 1, j: 2},
+				{i: 1, j: 2},
+			},
+			checkPairs: []Pair{
+				{i: 1, j: 2},
+			},
+			expected: []bool{true},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			set := NewHashSet()
+
+			for _, pair := range tt.intputPairs {
+				set.Add(pair.i, pair.j)
+			}
+
+			for idx, pair := range tt.checkPairs {
+				if got := set.Contains(pair.i, pair.j); got != tt.expected[idx] {
+					t.Errorf("Contains (%v) : %v; want %v", pair, got, tt.expected[idx])
+				}
+			}
+		})
+	}
+
 }
