6b74c9641cce7841995639d136bc7b18b267822a
michael <michael.g.george@gmail.com>
2024-11-21 10:01:09 +0530

    Create MostProfit fn in most profitable path

--- learn/algorithms/dynamic_programming/problems/most_profitable_path/main.go
+++ learn/algorithms/dynamic_programming/problems/most_profitable_path/main.go
@@ -30,3 +30,35 @@ func max(a, b int) int {
 	}
 	return a
 }
+
+func MostProfit(cost [][]int) int {
+	m := len(cost)
+	if m == 0 {
+		return 0
+	}
+	n := len(cost[0])
+
+	if n == 0 {
+		return 0
+	}
+
+	dp := make([][]int, m)
+	for i := 0; i < m; i++ {
+		dp[i] = make([]int, n)
+	}
+	dp[0][0] = cost[0][0]
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if i-1 >= 0 && j-1 >= 0 {
+				dp[i][j] = max(dp[i][j-1], dp[i-1][j]) + cost[i][j]
+			} else if i-1 >= 0 {
+				dp[i][j] = dp[i-1][j] + cost[i][j]
+			} else if j-1 >= 0 {
+				dp[i][j] = dp[i][j-1] + cost[i][j]
+			}
+
+		}
+	}
+	return dp[m-1][n-1]
+}
