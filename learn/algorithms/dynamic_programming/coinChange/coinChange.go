c125c5b957043362bdc50846a6a809b72032a08a
michael <michael.g.george@gmail.com>
2024-11-25 19:40:54 +0530

    Add exactlyCCoins to coinChange

--- learn/algorithms/dynamic_programming/coinChange/coinChange.go
+++ learn/algorithms/dynamic_programming/coinChange/coinChange.go
@@ -42,3 +42,50 @@ func coinChange(denominations []int, n int) int {
 	}
 	return dp[n]
 }
+
+/*
+Problem:
+
+	Find the total number of ways to make change n
+	by using exactly c coins.
+
+1.	Objective Function:
+		F(i, c) is the total number of ways to make
+		change i with exactly c coins.
+
+2.	Base Case:
+		F(i, 0) = 0
+		F(0, 0) = 1
+
+		if i in denominations:
+			F(i, 1) = 1
+		else:
+			F(i, 1) = 0
+
+3. 	Transition function:
+
+		for j in denominations:
+			if i >= j:
+				F(i, c) += F(i - j, c - 1)
+
+*/
+
+func exactlyCCoins(n int, c int, denominations []int) int {
+	dp := make([][]int, n+1)
+	for i := range dp {
+		dp[i] = make([]int, c+1)
+	}
+
+	dp[0][0] = 1
+
+	for _, denomination := range denominations {
+		for i := 1; i <= n; i++ {
+			for j := 1; j <= c; j++ {
+				if i >= denomination {
+					dp[i][j] += dp[i-denomination][j-1]
+				}
+			}
+		}
+	}
+	return dp[n][c]
+}
