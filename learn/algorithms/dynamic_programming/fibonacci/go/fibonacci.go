550608af4b958cb1b638057685f1016528b651cf
michael <michael.g.george@gmail.com>
2024-11-25 13:47:52 +0530

    Add bottom up forward solution to fibonacci/go

--- learn/algorithms/dynamic_programming/fibonacci/go/fibonacci.go
+++ learn/algorithms/dynamic_programming/fibonacci/go/fibonacci.go
@@ -31,3 +31,22 @@ func fibTopDownHelper(n int, memo map[int]int) int {
 	memo[n] = fibTopDownHelper(n-1, memo) + fibTopDownHelper(n-2, memo)
 	return memo[n]
 }
+
+// Bottom-up (forward dynamic programming)
+func BottomUpForward(n int) int {
+	if n == 0 {
+		return 0
+	}
+
+	if n <= 2 {
+		return 1
+	}
+
+	dp := make([]int, n+1)
+	dp[0] = 0
+	dp[1] = 1
+	for i := 2; i <= n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+	return dp[n]
+}
