115bea3cd4d39e32a95a802560497a7b0e6130e6
michael <michael.g.george@gmail.com>
2024-11-25 13:30:45 +0530

    Add top down solution to fibonacci/go

--- learn/algorithms/dynamic_programming/fibonacci/go/fibonacci.go
+++ learn/algorithms/dynamic_programming/fibonacci/go/fibonacci.go
@@ -10,3 +10,24 @@ func fib(n int) int {
 	}
 	return fib(n-1) + fib(n-2)
 }
+
+// Top-down solution: (memoization + recursion)
+func fibTopDown(n int) int {
+	memo := make(map[int]int)
+	return fibTopDownHelper(n, memo)
+}
+
+func fibTopDownHelper(n int, memo map[int]int) int {
+	if n == 0 {
+		return 0
+	}
+	if n <= 2 {
+		return 1
+	}
+	if res, ok := memo[n]; ok {
+		return res
+	}
+
+	memo[n] = fibTopDownHelper(n-1, memo) + fibTopDownHelper(n-2, memo)
+	return memo[n]
+}
