b36d2aec60b68305ff4839252426eb0e0f20f7c8
michael <michael.g.george@gmail.com>
2024-11-25 13:48:15 +0530

    Test bottom up forward solution to fibonacci/go

--- learn/algorithms/dynamic_programming/fibonacci/go/fibonacci_test.go
+++ learn/algorithms/dynamic_programming/fibonacci/go/fibonacci_test.go
@@ -64,3 +64,28 @@ func Test_fibTopDown(t *testing.T) {
 		})
 	}
 }
+
+func TestBottomUpForward(t *testing.T) {
+	tests := []struct {
+		name string
+		n    int
+		want int
+	}{
+		{name: "fib(0)", n: 0, want: 0},
+		{name: "fib(1)", n: 1, want: 1},
+		{name: "fib(2)", n: 2, want: 1},
+		{name: "fib(3)", n: 3, want: 2},
+		{name: "fib(4)", n: 4, want: 3},
+		{name: "fib(5)", n: 5, want: 5},
+		{name: "fib(6)", n: 6, want: 8},
+		{name: "fib(10)", n: 10, want: 55},
+		{name: "fib(20)", n: 20, want: 6765},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := BottomUpForward(tt.n); got != tt.want {
+				t.Errorf("BottomUpForward(%v) = %v, want %v", tt.n, got, tt.want)
+			}
+		})
+	}
+}
